93dcf81bd2adc9d7d85d7debb7f2e4d9f5ee8b2c
John Cai <CaiJianbin93@gmail.com>
2018-09-21 13:59:40 +0800

    priority zhengwu

--- xsp/src/xxlPages/insidePage.lua
+++ xsp/src/xxlPages/insidePage.lua
@@ -11,14 +11,47 @@ local function enterShiYe() --师爷
 	tap(x, y)
 end
 
-local function shouCai()
-	x, y = findImageInRegionFuzzy("zhengwu_zhengji.png", 95, 0, 0, 719, 1279, 0xffffff);
-	if x ~= -1  and y ~= -1 then       
-		tap(x,y)
-	else                               --如果没找到符合条件的
-		dialog("没找到 ╮（╯▽╰）╭",0);
+
+local pngTable = {
+	["yinliang"] = "zhengwu_yinliang.png",
+	["zhengji"] = "zhengwu_zhengji.png",
+	["shibin"] = "zhengwu_shibin.png",
+	["liangshi"] = "zhengwu_liangshi.png",
+	["shuxing"] = "zhengwu_shuxing.png"
+}
+
+--e.g. priorityArray = {"yinliang", "zhengji"}
+local function shouCai(priorityArray)
+	priorityArray[#priorityArray+1] = "zhengji" --fall back to zhengji
+	
+	local function findZhengWuOption(option)
+		local x, y = -1, -1
+		local png = pngTable[option]
+		if png ~= nil then
+			x, y = findImageInRegionFuzzy(png, 95, 0, 0, 719, 1279, 0xffffff);
+		end
+		return x, y
+	end
+	
+	while true do
+		local anyZhengWu = false
+		for i, v in ipairs(priorityArray) do
+			mSleep(500)
+			x, y = findZhengWuOption(v)
+			if x ~= -1 then
+				anyZhengWu = true
+				tap(x, y)
+				break
+			end
+		end
+		mSleep(1000)
+		if anyZhengWu == false then
+			break
+		end
 	end
 end
+
+
 
 local function exitShiYe()
 end
@@ -26,7 +59,9 @@ end
 function insidePage.zhengWu() --政务
 	enterShiYe()
 	mSleep(1000)
-	shouCai()
+	
+	local priorityArray = {"yinliang", "zhengji"}
+	shouCai(priorityArray)
 	exitShiYe()
 end
 
